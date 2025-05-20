a64359ea4d9ddcb818ffc23aef50fb6d2dfc9ee8
u03n0 <u03n0@proton.me>
2025-05-20 11:24:29 +0200

    using new tempalte function for train test split

--- src/models/bayesian/main.cpp
+++ src/models/bayesian/main.cpp
@@ -2,26 +2,26 @@
 #include <unordered_map>
 #include <vector>
 
-#include "ml_from_scratch_cpp/models/bayesian/naive_bayes.h" 
-#include "ml_from_scratch_cpp/core/data/reading.h"
-#include "ml_from_scratch_cpp/core/data/splitting.h"
-#include "ml_from_scratch_cpp/core/data/text.h"
+#include "models/bayesian/naive_bayes.h" 
+#include "core/data/reading.h"
+#include "core/data/splitting.h"
+#include "core/data/text.h"
 
 using std::vector;
 using std::unordered_map;
 using std::string;
-using vec_of_vec_str_map = vector<unordered_map<string, vector<string>>>; 
 
+typedef vector<unordered_map<string, string>> str_omap_vector_t;
+typedef vector<unordered_map<string, vector<string>>> dataset_vec_str_t;
 
 int main() {
 
  string filename = std::string(DATA_DIR) + "email.csv";
- vec_of_str_map data;
+ str_omap_vector_t data;
  data = read_csv(filename);
- vector<unordered_map<string, vector<string>>> processed = tokenize_dataset(data);
+ dataset_vec_str_t processed = tokenize_dataset(data);
 
- vec_of_vec_str_map train_data, test_data;
- std::tie(train_data, test_data) = train_test_split(processed, 0.8);
+ auto[train_data, test_data] = train_test_split(processed, 0.8);
 
  NaiveBayes nb;
  nb.fit(train_data);
--- src/models/knn/main.cpp
+++ src/models/knn/main.cpp
@@ -3,35 +3,32 @@
 #include <vector>
 #include <random>
 
-#include "ml_from_scratch_cpp/core/data/reading.h"
-#include "ml_from_scratch_cpp/core/data/splitting.h"
-#include "ml_from_scratch_cpp/core/data/text.h"
-#include "ml_from_scratch_cpp/core/encoding/text_terms.h"
-#include "ml_from_scratch_cpp/models/knn/knn.h"
+#include "core/data/reading.h"
+#include "core/data/splitting.h"
+#include "core/data/text.h"
+#include "core/encoding/text_terms.h"
+#include "models/knn/knn.h"
 using std::vector;
 using std::unordered_map;
 using std::string;
-using vec_of_vec_str_map = vector<unordered_map<string, vector<string>>>; 
 
+typedef vector<unordered_map<string, string>> str_omap_vector_t;
+typedef vector<unordered_map<string, vector<string>>> dataset_vec_str_t;
+typedef vector<unordered_map<string, vector<double>>> dataset_vec_double_t;
 
 int main() {
 
- string filename = std::string(DATA_DIR) + "email.csv";
- vec_of_str_map data;
+ string filename = string(DATA_DIR) + "email.csv";
+ str_omap_vector_t data;
  data = read_csv(filename);
- vector<unordered_map<string, vector<string>>> processed = tokenize_dataset(data);
+ dataset_vec_str_t processed = tokenize_dataset(data);
 
 
- vector<unordered_map<string, vector<double>>> encoded_data;
+ dataset_vec_double_t encoded_data;
  encoded_data = get_tf_idf(processed);
- 
- std::random_device rd;
- double ratio {0.8};
-  std::default_random_engine rng(rd());
-  std::shuffle(encoded_data.begin(), encoded_data.end(),  rng);
-  double split_index {ratio * encoded_data.size()};
-  vector<unordered_map<string, vector<double>>> train_data {encoded_data.begin(), encoded_data.begin() + (int) split_index};
-  vector<unordered_map<string, vector<double>>> test_data {encoded_data.begin() + (int) split_index, encoded_data.end()};
+
+ auto [train_data, test_data] = train_test_split(encoded_data, 0.8);
+
  KNN knn;
  knn.fit(train_data);
  int correct {knn.predict(test_data)};
